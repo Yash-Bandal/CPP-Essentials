0e6e9e1d5c08cd612db39f9f374a87918021bbb0
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-01-01 20:30:37 +0530

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here aw
+    std::cout << "Hello world!";
+    std::cout<<"Hi";
+
+    return 0;
+}
