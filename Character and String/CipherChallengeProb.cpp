3661211d3e053192ab3ae222e0d3cf8e335896ee
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-01-03 21:21:34 +0530

    CipherChallengeProb.cpp

--- Character and String/CipherChallengeProb.cpp
+++ Character and String/CipherChallengeProb.cpp
@@ -1,4 +1,3 @@
-
 // Section 10
 // Challenge - Solution
 // Substitution Cipher
@@ -30,55 +29,85 @@ Remember, the less code you write the less code you have to test!
 Reuse existing functionality in libraries and in the std::string class!
 */
 
-#include <iostream>
-#include <string>
 
+#include<iostream>
+#include<string>
 using namespace std;
-
-int main() {
+int main()
+{
+    string alphabet={" [abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    string key={"[ XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
     
-    string alphabet {"[ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    string key  {" [XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
-   
-    string secret_message {};
-    cout << "Enter your secret message : ";
-    getline(cin, secret_message);
+    string secret_msg;
+    string encrypted_msg;
+    string decrypted_msg;
     
-    string encrypted_message ;
     
-    cout << "\nEncrypting message..." << endl;
+    //get message
+    cout<<"Enter Message secretely.." ;
+    getline(cin,secret_msg);   
+    cout<<"Your secret Message is: "<<secret_msg<<endl;
     
-    for (char c: secret_message)
+    //access
+    cout<<endl;
+    cout<<"Encrypting.........."<<endl;
+    cout<<endl;
+    
+    //ENCRYPTION
+    for(char c:secret_msg)
     {
-        size_t position = alphabet.find(c);
-        if (position != string::npos) 
-        {
-            char new_char =  key.at(position) ;
-            encrypted_message += new_char;
-        } 
-        else
-        {
-            encrypted_message += c;
-        }
+      size_t position=alphabet.find(c);
+      //cout<<position<<endl;
+      if(position!=string::npos)
+      {
+          char new_char=key.at(position);
+          encrypted_msg+=new_char;
+          
+      }
+      else
+      {
+          encrypted_msg+=c; //if no key..return same character in encrypted
+      }
     }
+    cout<<"Yout Message Encrypted is: "<<encrypted_msg<<endl;
     
-    cout << "\nEncrypted message: " << encrypted_message << endl;
     
-    string decrypted_message {};
-    cout << "\nDecrypting message..." << endl;
+    cout<<endl;
+    cout<<"Decrypting.........."<<endl;
+    cout<<endl;
     
-    for (char c: encrypted_message) {
-        size_t position = key.find(c);
-        if (position != string::npos) {
-            char new_char { alphabet.at(position) };
-            decrypted_message += new_char;
-        } else {
-            decrypted_message += c;
-        }
+    
+    //DECRYPTION
+    for(char c:encrypted_msg) //iterate through encrypted msg
+    {
+      size_t position=key.find(c);
+      //cout<<position<<endl;
+      if(position!=string::npos)
+      {
+          char new_char=alphabet.at(position);
+          decrypted_msg+=new_char;
+          
+      }
+      else
+      {
+          decrypted_msg+=c; //if no key..return same character in encrypted
+      }
     }
+    cout<<"Yout Message Decrypted  is: "<<decrypted_msg<<endl;
     
-    cout << "\nDecrypted message: " << decrypted_message << endl;
     
-    cout << endl;
     return 0;
 }
+
+
+
+//Logic  Use of .find to get positon and .at to retrive character from position 
+/*
+1 Initialize dictionary for each alphabet ..which key ,also space
+2 First take in input string to encrypt
+3 acces each character in the "string" using for loop
+4 initialize position and get position using alphabet.find(character)
+5 if not string::npos gives final size..that is ..verify if string not reached to final 
+6.else store the key of the alphabet in string in new variable using key.at(positon)
+7 repeat ulta for decrypt
+*/
