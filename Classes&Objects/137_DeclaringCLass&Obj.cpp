79b78f4698c1789e62782d9f8684dd2a81f75ea3
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-01-14 21:54:27 +0530

    Create 137_DeclaringCLass&Obj.cpp

---
+++ Classes&Objects/137_DeclaringCLass&Obj.cpp
@@ -0,0 +1,54 @@
+// Section 13
+// Declare Classes and Objects
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Player {
+    // attributes
+    string name ="Player";
+    int health =100;
+    int xp =3;
+    
+    // methods
+    void talk(string);
+    bool is_dead();
+};
+
+class Account 
+{
+    // attributes
+    string name ="Account";
+    double balance =0.0;
+    
+    // methods
+    bool deposit(double);
+    bool withdraw(double);
+    
+};
+
+
+int main()
+{
+    Account frank_account;
+    Account jim_account;  //object declaration
+    
+    
+    Player frank;    //object declaration
+    Player hero;
+    
+    Player players[] {frank, hero};  //object_array dec
+    
+    vector<Player> player_vec {frank};  //object vector dec
+    player_vec.push_back(hero);
+    
+    
+    Player *enemy =nullptr;  //object pointer dec
+    enemy = new Player;
+    
+    delete enemy;
+
+    return 0;
+}
