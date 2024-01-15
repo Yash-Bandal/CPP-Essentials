f526aeb148643ab463827f8a023767a49a3db805
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-01-15 20:10:19 +0530

    Update 140_MemberFunctions&HeaderFiles.cpp

--- Classes&Objects/140_MemberFunctions&HeaderFiles.cpp
+++ Classes&Objects/140_MemberFunctions&HeaderFiles.cpp
@@ -122,28 +122,67 @@ int main() {
 #endif
 
 
+//=============================================================================================
+
+
+//Account.h
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+#include <string>
+
+class Account {
+private:
+    // attributes
+    std::string name;
+    double balance;
+    
+public:
+    // methods
+    // declared inline
+    void set_balance(double bal) { balance = bal; }
+    double get_balance() { return balance; }
+    
+    // methods will be declared outside the class declaration
+    void set_name(std::string n);
+    std::string get_name();
+    
+    bool deposit(double amount);
+    bool withdraw(double amount);
+};
+
+#endif // _ACCOUNT_H_
 
 
 
+//Account.cpp
+#include "Account.h"
+
+void Account::set_name(std::string n) {
+    name = n;
+}
+
+std::string Account::get_name() {
+    return name;
+}
+
+bool Account::deposit(double amount) {
+    // if verify amount
+    balance += amount;
+    return true;
+}
+
+bool Account::withdraw(double amount) {
+    if (balance-amount >= 0) {
+        balance -= amount;
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//main.cpp
 #include <iostream>
 #include "Account.h"
 
