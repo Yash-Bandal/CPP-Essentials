792d551f7a76481b334c2237c20ab44c52d540c6
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-01-15 19:28:36 +0530

    Create 139_AccessModifierorSpecifier.cpp

---
+++ Classes&Objects/139_AccessModifierorSpecifier.cpp
@@ -0,0 +1,26 @@
+// Section 13
+// Access Modifers
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player {
+private:
+    // attributes
+    string name {"Player"};
+    int health;
+    int xp;
+public:
+    // methods
+    void talk(string text_to_say) { cout << name << " says " << text_to_say << endl;  }
+    bool is_dead();
+};
+
+int main() {
+    Player frank;
+    // frank.name = "Frank";               // Compiler error
+    // cout << frank.health << endl;       // Compiler error
+    frank.talk("Hello there");
+    return 0;
+}
